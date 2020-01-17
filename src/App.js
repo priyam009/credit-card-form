719c5029c27f9097f17082bfb861d92dfe6bb30d
Priyam <priyam009@live.com>
2020-01-18 10:12:54 +1100

    Import FormData from Components into App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import { CardDiv, CardImage } from "./components/Card";
-import { FormDiv } from "./components/Form";
+import { FormDiv, FormData } from "./components/Form";
 
 class App extends React.Component {
   render() {
