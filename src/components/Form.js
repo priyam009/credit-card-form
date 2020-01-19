e26b5f013cfd1ccc8c484eb564dbcaa96eb49e24
Priyam <priyam009@live.com>
2020-01-19 22:33:09 +1100

    Add and rearrange into components various sections of the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,31 +2,68 @@ import React from "react";
 import "../App.css";
 
 export function FormDiv(props) {
-return <div className="form common">{props.children}</div>;
+  return <div className="form common">{props.children}</div>;
 }
 
 export function FormData(props) {
+  return <form className="form-data">{props.children}</form>;
+}
+
+export function CardNumber(props) {
   return (
-    <form className="form-data">
-      <label className="form-data-label-block">
-        Card Number
-        <input type="text" placeholder="Credit Card Number" />
-      </label>
-      <label className="form-data-label-block">
-        Card Name
-        <input type="text" placeholder="Card Holder Name" />
-      </label>
-      <div className="form-data-div-inline">
-        <label>
-          Expiration Date
-          <input type="text" placeholder="Month"/>
-          <input type="text" placeholder="Year" />
-        </label>
-        <label>
-          CVV
-          <input type="text" />
-        </label>
+    <label className="form-data-label-block">
+      Card Number
+      <input
+        className="form-data-input form-data-input-common"
+        type="text"
+        placeholder="Credit Card Number"
+      />
+    </label>
+  );
+}
+
+export function CardName(props) {
+  return (
+    <label className="form-data-label-block">
+      Card Name
+      <input
+        className="form-data-input form-data-input-common"
+        type="text"
+        placeholder="Card Holder Name"
+      />
+    </label>
+  );
+}
+
+export function FormInline(props) {
+  return <div className="form-data-div-inline">{props.children}</div>;
+}
+
+export function ExpiryDate(props) {
+  return (
+    <label>
+      Expiration Date
+      <div className="form-expiry">
+        <input
+          type="text"
+          className="form-data-input-common"
+          placeholder="Month"
+        />
+        <input
+          type="text"
+          className="form-data-input-common"
+          placeholder="Year"
+        />
       </div>
-    </form>
+    </label>
+  );
+}
+
+export function CVV(props) {
+  return (
+    <label>
+      CVV
+      <input className="form-data-input form-data-input-common" type="text" />
+    </label>
   );
 }
