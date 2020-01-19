27d7df81c11b3e3012496af19c2e1181d7accf17
Priyam <priyam009@live.com>
2020-01-19 22:31:32 +1100

    Import child components for Form into App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import { CardDiv, CardImage } from "./components/Card";
-import { FormDiv, FormData } from "./components/Form";
+import {
+  FormDiv,
+  FormData,
+  CardName,
+  CardNumber,
+  FormInline,
+  ExpiryDate,
+  CVV
+} from "./components/Form";
 
 class App extends React.Component {
   render() {
