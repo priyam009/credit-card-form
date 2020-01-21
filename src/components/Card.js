337c087dad5ba29b7edfb36a5fd47123fd85bf47
Priyam <priyam009@live.com>
2020-01-21 22:57:24 +1100

    Add CardNumber, CardName and CardExpiry code

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,32 @@ import React from "react";
 import "../App.css";
 import credit from "../credit-background.jpg";
 
-
 export function CardDiv(props) {
   return <div className="card common">{props.children}</div>;
 }
 
 export function CardImage(props) {
   return <img className="card-image" alt="sample" src={credit}></img>;
+}
+
+export function CardNumber(props) {
+  return <div className="cardnumber common-card">XXXX XXXX XXXX XXXX</div>;
+}
+
+export function CardName(props) {
+  return (
+    <div className="cardname common-card">
+      <div>Card Holder</div>
+      <div>Full Name</div>
+    </div>
+  )
+}
+
+export function CardExpiry(props) {
+  return (
+    <div className="expirydate common-card">
+      <div>Expiry Date</div>
+      <div>MM/YY</div>
+    </div>
+  )
 }
